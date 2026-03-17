28d7a384afffa890374fd864fe7f8f9279474ed8
andrey123salgueiro <salgueiro.andrey@escola.pr.gov.br>
2026-03-17 08:21:15 -0300

    Add files via upload

---
+++ filtrar.js
@@ -0,0 +1,17 @@
+const usuarios = [
+    { nome: "Carlos", idade: 32},
+    { nome: "Ana", idade: 28},
+    { nome: "Felipe", idade: 40}
+];
+
+// Encontrando um objeto
+const ana = usuarios.find(usuario => usuario.nome === "Ana");
+console.log(ana);
+
+//filtrando o objeto
+const usuariosAcimaDe30 = usuarios.filter(usuario => usuario.idade > 30);
+console.log(usuariosAcimaDe30);
+
+// ordenando os objetos por idade
+const usuariosOrdenadosPorIdade = usuarios.sort((a, b) => a.idade - b.idade);
+console.log(usuariosOrdenadosPorIdade); 
