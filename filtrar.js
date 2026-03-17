70cc8d6ef947438470c2e73e316d8f22fdce1bad
andrey123salgueiro <salgueiro.andrey@escola.pr.gov.br>
2026-03-17 08:46:03 -0300

    Add sorting of users by descending age
    Added sorting of users by descending age.

--- filtrar.js
+++ filtrar.js
@@ -15,3 +15,7 @@ console.log(usuariosAcimaDe30);
 // ordenando os objetos por idade
 const usuariosOrdenadosPorIdade = usuarios.sort((a, b) => a.idade - b.idade);
 console.log(usuariosOrdenadosPorIdade); 
+
+// ordenando os objetos por idade
+const usuariosOrdenadosPorIdades = usuarios.sort((a, b) => b.idade - a.idade);
+console.log(usuariosOrdenadosPorIdade); 
